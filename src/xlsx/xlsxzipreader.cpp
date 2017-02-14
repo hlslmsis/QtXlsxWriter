a7ab7b51fca578c025eb8f2fda5df01db6215bd6
Stivius <maxim11853@gmail.com>
2017-02-14 18:55:36 +0200

    Fixed bug

--- src/xlsx/xlsxzipreader.cpp
+++ src/xlsx/xlsxzipreader.cpp
@@ -48,7 +48,7 @@ ZipReader::~ZipReader()
 
 void ZipReader::init()
 {
-    QList<QZipReader::FileInfo> allFiles = m_reader->fileInfoList();
+    QList<QZipReader::FileInfo> allFiles = m_reader->fileInfoList().toList();
     foreach (const QZipReader::FileInfo &fi, allFiles) {
         if (fi.isFile)
             m_filePaths.append(fi.filePath);
